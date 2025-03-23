9e9003667acc6cb662b5458253f636c14fe4f3c6
Germain So <germain717@gmail.com>
2025-03-23 15:23:14 -0700

    fix date picker bug by using normal input method

--- src/features/tasks/components/create-task-form.tsx
+++ src/features/tasks/components/create-task-form.tsx
@@ -1,9 +1,9 @@
-import { createTaskSchema } from "../schemas";
-import { useCreateTask } from "../api/use-create-tasks";
-import { DatePicker } from "@/components/date-picker";
-import { MemberAvatar } from "@/features/members/components/member-avatar"; // TODO: FB-3025
-import { TaskStatus } from "../types";
-import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import {createTaskSchema} from "../schemas";
+import {useCreateTask} from "../api/use-create-tasks";
+import {DatePicker} from "@/components/date-picker";
+import {MemberAvatar} from "@/features/members/components/member-avatar"; // TODO: FB-3025
+import {TaskStatus} from "../types";
+import {ProjectAvatar} from "@/features/projects/components/project-avatar";
 import {
     Select,
     SelectContent,
@@ -11,18 +11,18 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
-import { useRouter } from "next/navigation";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, FormProvider, Controller } from "react-hook-form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { DottedSeparator } from "@/components/ui/dotted-separator";
-import { cn } from "@/lib/utils";
-import { useRef } from "react";
+import {useWorkspaceId} from "@/features/workspaces/hooks/use-workspace-id";
+import {useRouter} from "next/navigation";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm, FormProvider, Controller} from "react-hook-form";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {DottedSeparator} from "@/components/ui/dotted-separator";
+import {cn} from "@/lib/utils";
+import {useRef} from "react";
 
 interface CreateTaskFormProps {
     onCancel?: () => void;
@@ -30,19 +30,19 @@ interface CreateTaskFormProps {
     memberOptions: { id: string; name: string }[];
 }
 
-export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: CreateTaskFormProps) => {
+export const CreateTaskForm = ({onCancel, projectOptions, memberOptions}: CreateTaskFormProps) => {
     const workspaceId = useWorkspaceId();
     const router = useRouter();
-    const { mutate, isPending } = useCreateTask();
+    const {mutate, isPending} = useCreateTask();
 
     const form = useForm<z.infer<typeof createTaskSchema>>({
-        resolver: zodResolver(createTaskSchema.omit({ workspaceId: true })),
-        defaultValues: { workspaceId },
+        resolver: zodResolver(createTaskSchema.omit({workspaceId: true})),
+        defaultValues: {workspaceId},
     });
 
     const onSubmit = (values: z.infer<typeof createTaskSchema>) => {
         mutate(
-            { json: { ...values, workspaceId } },
+            {json: {...values, workspaceId}},
             {
                 onSuccess: () => {
                     form.reset();
@@ -59,7 +59,7 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
             </CardHeader>
 
             <div className="px-7">
-                <DottedSeparator />
+                <DottedSeparator/>
             </div>
 
             <CardContent className="p-7">
@@ -70,13 +70,13 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                             <FormField
                                 control={form.control}
                                 name="name"
-                                render={({ field }) => (
+                                render={({field}) => (
                                     <FormItem>
                                         <FormLabel>Task Name</FormLabel>
                                         <FormControl>
-                                            <Input {...field} placeholder="Enter task name" />
+                                            <Input {...field} placeholder="Enter task name"/>
                                         </FormControl>
-                                        <FormMessage />
+                                        <FormMessage/>
                                     </FormItem>
                                 )}
                             />
@@ -85,13 +85,13 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                             <FormField
                                 control={form.control}
                                 name="dueDate"
-                                render={({ field }) => (
+                                render={({field}) => (
                                     <FormItem>
-                                        <FormLabel>Due Date</FormLabel>
+                                        <FormLabel>Due date</FormLabel>
                                         <FormControl>
-                                            <DatePicker {...field} />
+                                            <Input {...field} placeholder="Enter due date yyyy-mm-dd"/>
                                         </FormControl>
-                                        <FormMessage />
+                                        <FormMessage/>
                                     </FormItem>
                                 )}
                             />
@@ -100,27 +100,27 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                             <FormField
                                 control={form.control}
                                 name="assigneeId"
-                                render={({ field }) => (
+                                render={({field}) => (
                                     <FormItem>
                                         <FormLabel>Assignee</FormLabel>
                                         <Select defaultValue={field.value} onValueChange={field.onChange}>
                                             <FormControl>
                                                 <SelectTrigger>
-                                                    <SelectValue placeholder="Select assignee" />
+                                                    <SelectValue placeholder="Select assignee"/>
                                                 </SelectTrigger>
                                             </FormControl>
                                             <SelectContent>
                                                 {memberOptions.map((member) => (
                                                     <SelectItem key={member.id} value={member.id}>
                                                         <div className="flex items-center gap-x-2">
-                                                            <MemberAvatar className="size-6" name={member.name} />
+                                                            <MemberAvatar className="size-6" name={member.name}/>
                                                             {member.name}
                                                         </div>
                                                     </SelectItem>
                                                 ))}
                                             </SelectContent>
                                         </Select>
-                                        <FormMessage />
+                                        <FormMessage/>
                                     </FormItem>
                                 )}
                             />
@@ -129,13 +129,13 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                             <FormField
                                 control={form.control}
                                 name="status"
-                                render={({ field }) => (
+                                render={({field}) => (
                                     <FormItem>
                                         <FormLabel>Status</FormLabel>
                                         <Select defaultValue={field.value} onValueChange={field.onChange}>
                                             <FormControl>
                                                 <SelectTrigger>
-                                                    <SelectValue placeholder="Select status" />
+                                                    <SelectValue placeholder="Select status"/>
                                                 </SelectTrigger>
                                             </FormControl>
                                             <SelectContent>
@@ -146,7 +146,7 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                                                 <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
                                             </SelectContent>
                                         </Select>
-                                        <FormMessage />
+                                        <FormMessage/>
                                     </FormItem>
                                 )}
                             />
@@ -155,13 +155,13 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                             <FormField
                                 control={form.control}
                                 name="projectId"
-                                render={({ field }) => (
+                                render={({field}) => (
                                     <FormItem>
                                         <FormLabel>Project</FormLabel>
                                         <Select defaultValue={field.value} onValueChange={field.onChange}>
                                             <FormControl>
                                                 <SelectTrigger>
-                                                    <SelectValue placeholder="Select project" />
+                                                    <SelectValue placeholder="Select project"/>
                                                 </SelectTrigger>
                                             </FormControl>
                                             <SelectContent>
@@ -179,13 +179,13 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
                                                 ))}
                                             </SelectContent>
                                         </Select>
-                                        <FormMessage />
+                                        <FormMessage/>
                                     </FormItem>
                                 )}
                             />
                         </div>
 
-                        <DottedSeparator className="py-7" />
+                        <DottedSeparator className="py-7"/>
 
                         <div className="flex items-center justify-between">
                             <Button
